6951c41e39380cd5801cf167d3785b89e54d343b
Nahidahmed <nahid.mansuri@gmail.com>
2021-01-01 14:21:51 +0530

    Update webScrapper.py

--- webScrapper.py
+++ webScrapper.py
@@ -4,4 +4,16 @@ from bs4 import BeautifulSoup
 url = 'http://quotes.toscrape.com'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
-print(soup)
+
+
+quotes = soup.find_all('span','text')
+authors = soup.find_all('small', 'author')
+tags = soup.find_all('div','tags')
+
+for i in range(0,len(quotes)):
+    print(quotes[i].text)
+    print(authors[i].text)
+    quoteTags = tags[i].find_all('a','tag')
+    for quoteTag in quoteTags:
+        print(quoteTag.text)
+
